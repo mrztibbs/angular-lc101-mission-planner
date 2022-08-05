1cc251252cc35bdad40a760b0ef7127a7541a9d9
April G Tibbs <mrztibbs@gmail.com>
2022-08-04 20:54:30 -0500

    Finished studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -17,5 +17,25 @@ export class CrewComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  memberBeingEdited: object = null;
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }  
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+ 
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+  }
 
 }
